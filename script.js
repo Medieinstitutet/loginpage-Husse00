4ee1b415dc102b9c5865ea3e7561bb9258421788
Husse00 <94406339+Husse00@users.noreply.github.com>
2021-12-01 21:50:56 +0100

    log

---
+++ script.js
@@ -0,0 +1,11 @@
+let root = document.getElementById("root");
+// navigation och header //
+let header = document.createElement("div")
+header.className += 'header1';
+let nav = document.createElement("nav")
+nav.innerText = "This is a login form project";
+nav.className += 'navigation';
+header.append(nav);
+root.append(header);
+
+// footer //
